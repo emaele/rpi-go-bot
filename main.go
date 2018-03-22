ad2108f581133fe03ae43bdb2791b8cc473d0694
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2018-03-22 16:29:48 +0100

    Constantly checks for CPU's temperature, set a temperature limit to get notified

--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 var (
-	myID     int64 = 000000            // you should replace this with your id
-	tokenBot       = "Your token here" // get your token bot from BotFather
+	myID      int64   = 000000            // you should replace this with your id
+	tokenBot          = "Your token here" // get your token bot from BotFather
+	tempLimit float64 = 50                // temperature limit
 )
 
 func main() {
@@ -34,76 +36,78 @@ func main() {
 	boot := tgbotapi.NewMessage(myID, "@"+bot.Self.UserName+" is now up! 👌")
 	bot.Send(boot)
 
+	go tempAlert(tempLimit, bot)
+
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
+		go mainBot(bot, update)
+	}
+}
 
-		if update.Message.Chat.ID == myID {
-			if update.Message.IsCommand() {
-				msg := tgbotapi.NewMessage(myID, "")
-				switch update.Message.Command() {
-				case "start":
-					msg.Text = "Hi " + update.Message.From.FirstName + " 👋"
-				case "temp":
-					cmd := exec.Command("cat", "/sys/class/thermal/thermal_zone0/temp")
-					if output, err := getOut(cmd); err == nil {
-						log := strings.Split(output, "\n")
-						if temp, err := strconv.ParseFloat(log[0], 64); err == nil {
-							temp = temp / 1000
-							msg.Text = "Temperature is: " + fmt.Sprint(temp) + "°C 🔥"
-						} else {
-							msg.Text = "Error"
-						}
+func mainBot(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+
+	if update.Message.Chat.ID == myID {
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(myID, "")
+			switch update.Message.Command() {
+			case "start":
+				msg.Text = "Hi " + update.Message.From.FirstName + " 👋"
+			case "temp":
+				cmd := exec.Command("cat", "/sys/class/thermal/thermal_zone0/temp")
+				if output, err := getOut(cmd); err == nil {
+					log := strings.Split(output, "\n")
+					if temp, err := strconv.ParseFloat(log[0], 64); err == nil {
+						temp = temp / 1000
+						msg.Text = "Temperature is: " + fmt.Sprint(temp) + "°C 🔥"
 					} else {
 						msg.Text = "Error"
 					}
-				case "reboot":
-					cmd := exec.Command("reboot")
-					msg.Text = "Rebooting RPi! 🔄"
-					bot.Send(msg)
-					cmd.Run()
-				case "available_space":
-					cmd := exec.Command("df", "--output=avail", "/")
-					if output, err := getOut(cmd); err == nil {
-						log := strings.Split(output, "\n")
-						if value, err := strconv.Atoi(log[1]); err == nil {
-							msg.Text = "Available space: " + fmt.Sprint(value/1000000) + "GB 💾"
-						} else {
-							msg.Text = "Error"
-						}
-					} else {
-						msg.Text = "Error"
-					}
-				case "speedtest":
-					cmd := exec.Command("speedtest-cli")
-					if output, err := getOut(cmd); err == nil {
-						log := strings.Split(output, "\n")
-
-						var down, up string
-
-						for _, element := range log {
-							if strings.HasPrefix(element, "Download:") {
-								down = element
-							} else if strings.HasPrefix(element, "Upload:") {
-								up = element
-							}
-						}
-						msg.Text = "⬇️ " + down + "\n" + "⬆️ " + up
-					} else {
-						msg.Text = "Error"
-					}
-
-				default:
-					msg.Text = "I don't know that command"
+				} else {
+					msg.Text = "Error"
 				}
+			case "reboot":
+				cmd := exec.Command("reboot")
+				msg.Text = "Rebooting RPi! 🔄"
 				bot.Send(msg)
+				cmd.Run()
+			case "available_space":
+				cmd := exec.Command("df", "--output=avail", "/")
+				if output, err := getOut(cmd); err == nil {
+					msgSplit := strings.Split(output, "\n")
+					if value, err := strconv.Atoi(msgSplit[1]); err == nil {
+						msg.Text = "Available space: " + fmt.Sprint(value/1000000) + "GB 💾"
+					} else {
+						msg.Text = "Error"
+					}
+				} else {
+					msg.Text = "Error"
+				}
+			case "speedtest":
+				cmd := exec.Command("speedtest-cli")
+				if output, err := getOut(cmd); err == nil {
+					log := strings.Split(output, "\n")
+
+					var down, up string
+
+					for _, element := range log {
+						if strings.HasPrefix(element, "Download:") {
+							down = element
+						} else if strings.HasPrefix(element, "Upload:") {
+							up = element
+						}
+					}
+					msg.Text = "⬇️ " + down + "\n" + "⬆️ " + up
+				} else {
+					msg.Text = "Error"
+				}
+			default:
+				msg.Text = "I don't know that command"
 			}
-		} else {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			msg.Text = "You are not authorized to use this bot ⚠️"
 			bot.Send(msg)
 		}
+	} else {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		msg.Text = "You are not authorized to use this bot ⚠️"
+		bot.Send(msg)
 	}
 }
 
@@ -116,4 +120,28 @@ func getOut(command *exec.Cmd) (output string, fail error) {
 	output = string(stdoutStderr)
 
 	return output, fail
+}
+
+func tempAlert(limit float64, bot *tgbotapi.BotAPI) {
+
+	for {
+		cmd := exec.Command("cat", "/sys/class/thermal/thermal_zone0/temp")
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			println("Errore comando")
+		}
+		tmp := string(stdoutStderr)
+		log := strings.Split(tmp, "\n")
+		temptmp, err := strconv.ParseFloat(log[0], 64)
+
+		temp := temptmp / 1000
+
+		if temp >= limit {
+			msg := tgbotapi.NewMessage(myID, "")
+			msg.Text = "\t⚠️Attention please⚠️ \n🔥 Your RPi temperature is over " + fmt.Sprint(limit) + "°C 🔥"
+			bot.Send(msg)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
