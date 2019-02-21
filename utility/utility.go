a6fe84e2026954506cd8b12596ddb2e9a1a327a6
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-02-21 21:53:02 +0100

    move gettemp and rewrite available space

--- utility/utility.go
+++ utility/utility.go
@@ -2,34 +2,22 @@ package utility
 
 import (
 	"fmt"
-	"os/exec"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"gitlab.com/emaele/rpi-go-bot/commands"
 )
 
 // TempAlert monitors CPU temperature every 10 seconds and sends a message if it's over the limit
 func TempAlert(limit float64, myID int64, bot *tgbotapi.BotAPI) {
 
 	for {
-		actualTemp, _ := strconv.ParseFloat(GetTemp(), 64)
+		actualTemp, _ := strconv.ParseFloat(commands.GetTemp(), 64)
 		if actualTemp >= limit {
 			msg := tgbotapi.NewMessage(myID, "\t⚠️Attention please⚠️ \n🔥 Your RPi temperature is over "+fmt.Sprint(limit)+"°C 🔥")
 			bot.Send(msg)
 		}
 		time.Sleep(10 * time.Second)
 	}
-}
-
-// GetTemp gets the actual temperature of your rpi's CPU
-func GetTemp() (temp string) {
-
-	cmd := exec.Command("/opt/vc/bin/vcgencmd", "measure_temp")
-	if stdoutStderr, err := cmd.CombinedOutput(); err == nil {
-		log := string(stdoutStderr)
-		temp = strings.Trim(log, "temp='C\n")
-	}
-	return
 }
