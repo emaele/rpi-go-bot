3aee885932ee9ec57451bc3e3d667da3224b6623
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-03-09 20:09:37 +0100

    move pihole summary and use ticker for temp alert

--- utility/utility.go
+++ utility/utility.go
@@ -6,18 +6,33 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	gohole "github.com/shuienko/go-pihole"
 	"gitlab.com/emaele/rpi-go-bot/commands"
 )
 
 // TempAlert monitors CPU temperature every 10 seconds and sends a message if it's over the limit
 func TempAlert(limit float64, myID int64, bot *tgbotapi.BotAPI) {
 
-	for {
+	for range time.NewTicker(10 * time.Second).C {
 		actualTemp, _ := strconv.ParseFloat(commands.GetTemp(), 64)
 		if actualTemp >= limit {
 			msg := tgbotapi.NewMessage(myID, "\t⚠️Attention please⚠️ \n🔥 Your RPi temperature is over "+fmt.Sprint(limit)+"°C 🔥")
 			bot.Send(msg)
 		}
-		time.Sleep(10 * time.Second)
 	}
+}
+
+//GetPiholeSummary returns pihole current status and info as a string
+func GetPiholeSummary(ph gohole.PiHConnector) string {
+	summary := ph.Summary()
+
+	var status string
+
+	if summary.Status == "enabled" {
+		status = "enabled ✅"
+	} else {
+		status = "disabled 🛑"
+	}
+
+	return fmt.Sprintf("PiHole is currently %s\n\nPercentage blocked: %s%%\nDNS Queries: %s\nAds Blocked: %s", status, summary.AdsPercentage, summary.AdsBlocked, summary.DNSQueries)
 }
